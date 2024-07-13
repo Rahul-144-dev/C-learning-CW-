959e1f25908dabdacc77d76ddb3677e98cf5852a
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-07-13 13:20:38 +0530

    com

--- Functions/05_factorial.c
+++ Functions/05_factorial.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
+int factorial(int x)
+{
+    int fact = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
 int main()
 {
     int n, r;
     printf("enter number n : ");
     scanf("%d", &n); // it scans the n and store it to int n;
     printf("enter number r : ");
-    scanf("%d", &r); // it scans the r and store it to int r;
-    int nfact = 1, rfact = 1, nrfact = 1;
-    for (int i = 1; i <= n; i++)
-    {
-        nfact *= i;
-    }
-    // printf("%d\n", nfact);
-    for (int j = 1; j <= r; j++)
-    {
-        rfact *= j;
-    }
-    // printf("%d\n", rfact);
-    for (int i = 1; i <= n - r; i++)
-    {
-        nrfact *= i;
-    }
-    // printf("%d\n", nrfact);
-    int ncr = nfact / (rfact * nrfact);
+    scanf("%d", &r);                                 // it scans the r and store it to int r;
+    int n_fact = factorial(n);                       // this function gives the factorial of n number
+    int r_fact = factorial(r);                       // this function gives the factorial of r number
+    int n_r_fact = factorial(n - r);                 // this function gives the factorial of (n-r) number
+    int ncr /*!ⁿcᵣ*/ = n_fact / (r_fact * n_r_fact); // ⁿcᵣ->means n-factorial/(r-factorial × n-r's factorial)
     printf("%d", ncr);
     return 0;
 }
