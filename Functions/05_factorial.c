8d9156d7c6e3ca64accda0586904a1e70ddc7be4
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-07-13 20:35:19 +0530

    s

--- Functions/05_factorial.c
+++ Functions/05_factorial.c
@@ -8,17 +8,18 @@ int factorial(int x)
     }
     return fact;
 }
+int ncrf(int n, int r)
+{
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
 int main()
 {
     int n, r;
     printf("enter number n : ");
-    scanf("%d", &n); // it scans the n and store it to int n;
+    scanf("%d", &n);
     printf("enter number r : ");
-    scanf("%d", &r);                                 // it scans the r and store it to int r;
-    int n_fact = factorial(n);                       // this function gives the factorial of n number
-    int r_fact = factorial(r);                       // this function gives the factorial of r number
-    int n_r_fact = factorial(n - r);                 // this function gives the factorial of (n-r) number
-    int ncr /*!ⁿcᵣ*/ = n_fact / (r_fact * n_r_fact); // ⁿcᵣ->means n-factorial/(r-factorial × n-r's factorial)
+    scanf("%d", &r);
+    int ncr /*!ⁿcᵣ*/ = ncrf(n, r); // ⁿcᵣ->means n-factorial/(r-factorial × n-r's factorial)
     printf("%d", ncr);
     return 0;
 }
--- Pattern Printing/innertriangle.c
+++ Pattern Printing/innertriangle.c
@@ -1,25 +1,3 @@
-// #include <stdio.h>
-// int main()
-// {
-//     int n; // n=4
-//     printf("enter number : ");
-//     scanf("%d", &n);
-//     int a = n-1;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         int x = n - 1;
-//         for (int j = 1; j <= a; j++)
-//         {
-//             printf("%d ", x);
-//             x--;
-//         }
-//         x--;
-//         a--;
-//         printf("\n");
-//     }
-//     return 0;
-// }
-
 #include <stdio.h>
 int main()
 {
