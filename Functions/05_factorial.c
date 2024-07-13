b6e9631f9a3237ff49305a44a8041fa417b5869b
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-07-13 12:50:08 +0530

    comments

--- Functions/05_factorial.c
+++ Functions/05_factorial.c
@@ -3,27 +3,26 @@ int main()
 {
     int n, r;
     printf("enter number n : ");
-    scanf("%d", &n);
+    scanf("%d", &n); //! it scans the n and store it to int n;
     printf("enter number r : ");
-    scanf("%d", &r);
+    scanf("%d", &r); //! it scans the r and store it to int r;
     int nfact = 1, rfact = 1, nrfact = 1;
     for (int i = 1; i <= n; i++)
     {
         nfact *= i;
     }
-    // printf("%d\n", nfact);
+    //! printf("%d\n", nfact);
     for (int j = 1; j <= r; j++)
     {
         rfact *= j;
     }
-    // printf("%d\n", rfact);
+    //! printf("%d\n", rfact);
     for (int i = 1; i <= n - r; i++)
     {
         nrfact *= i;
     }
-    // printf("%d\n", nrfact);
+    //! printf("%d\n", nrfact);
     int ncr = nfact / (rfact * nrfact);
     printf("%d", ncr);
     return 0;
 }
-
