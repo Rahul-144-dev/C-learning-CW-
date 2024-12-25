178d5f1e59a797e9b253ee0b841bbe765c887825
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-12-25 20:43:44 +0530

    2d

--- 08_2D_Array/01_main.c
+++ 08_2D_Array/01_main.c
@@ -1,63 +1,25 @@
 #include <stdio.h>
-int row, col;
-// void input_array(int arr[row][col]);
-void input_rc(int *row, int *col);
-void input_2d(int array[row][col]);
-void print_array(int arr[row][col]);
-// void print_size(int (*arr)[row][col]);
-// Global Variable
+void print_array(int arr[2][3]);
+void print_size(int (*arr)[2][3]);
 void main()
 {
-  input_rc(&row, &col);
-  int arr[row][col];
-  input_2d(arr);
-  print_array(arr);
+  int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+  print_size(&arr);
+  // print_array(arr);
   return;
 }
-void input_rc(int *row, int *col)
+void print_array(int arr[2][3])
 {
-  printf("Enter Row : ");
-  scanf("%d", row);
-  printf("Enter Col : ");
-  scanf("%d", col);
-}
-void input_2d(int array[row][col])
-{
-  for (int i = 0; i < row; i++)
+  for (int i = 0; i < 2; i++)
   {
-    printf("R%d : ", i + 1);
-    for (int j = 0; j < col; j++)
-    {
-      printf("C%d : ", j + 1);
-      scanf("%d", &array[i][j]);
-    }
-  }
-}
-void input_array(int arr[row][col])
-{
-  puts("Enter array elements");
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < col; j++)
-    {
-      printf("arr[%d][%d] = ", i, j);
-      scanf("%d", &arr[i][j]);
-    }
-  }
-}
-void print_array(int arr[row][col])
-{
-  puts("the elements of the array");
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < col; j++)
+    for (int j = 0; j < 3; j++)
     {
       printf("%d ", arr[i][j]);
     }
     puts("");
   }
 }
-void print_size(int (*arr)[row][col])
+void print_size(int (*arr)[2][3])
 {
   int size_row = sizeof(arr) / sizeof(arr[0]);
   int size_col = sizeof(arr[0]) / sizeof(arr[0][0]);
@@ -65,6 +27,6 @@ void print_size(int (*arr)[row][col])
   printf("sizeof(arr[0])    = %d\n", sizeof(arr[0]));    // 12
   printf("sizeof(arr[0][0]) = %d\n", sizeof(arr[0][0])); // 4
   printf("size_row          = %d\n", size_row);          // 2
-  printf("size_col          = %d\n", size_col);          // row
+  printf("size_col          = %d\n", size_col);          // 3
   return;
 }
---
+++ 08_2D_Array/03_programe.c
@@ -0,0 +1,10 @@
+// Ques : Write a program to store roll number and
+// marks obtained by 4 students side by side in a
+// matrix.
+#include <stdio.h>
+int st_no, sub = 3;
+void main()
+{
+  int st_arr[st_no][sub];
+  
+}
