6a238119d1c5114dcef33e6871ac09a6d501b04b
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-12-29 18:25:01 +0530

    pr

--- 08_2D_Array/014_mul_matrix.c
+++ 08_2D_Array/014_mul_matrix.c
@@ -1,10 +1,87 @@
 #include <stdio.h>
-int r, c;
+int m, n, p, q;
+void get_dimension(int *m, int *n, int *p, int *q);
+void array_elements(int arr[m][n], int brr[p][q]);
+void matrix_multiply(int arr[m][n], int brr[p][q], int res[m][q]);
+void print_array(int res[m][q]);
 /* Main Function */
 int main()
 {
-  int arr[r][c];
-  
-  puts("");
+  do
+  {
+    get_dimension(&m, &n, &p, &q); // Prompt the user to enter dimensions
+    if (n != p)
+    {
+      printf("The matrices cannot be multiplied....Please Re-enter the dimensions again....\n");
+    }
+  } while (n != p); // Repeat the loop if the number of columns in the first matrix is not equal to the number of rows in the second matrix
+  // Rest of the code...
+  int arr[m][n];
+  int brr[p][q];
+  int res[m][q];
+  array_elements(arr, brr);
+  matrix_multiply(arr, brr, res);
+  print_array(res);
   return 0;
+}
+void matrix_multiply(int arr[m][n], int brr[p][q], int res[m][q])
+{
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < q; j++)
+    {
+      // i row of a[][] & j column of b[][]
+      // (a[i][0],a[i][1],a[i][2]......) * (b[0][j],b[1][j],b[2][j],)
+      // b[0][j],
+      res[i][j] = 0;
+      for (int k = 0; k < p; k++)
+      {
+        res[i][j] += arr[i][k] * brr[k][j];
+      }
+    }
+  }
+}
+void array_elements(int arr[m][n], int brr[p][q])
+{
+  printf("Enter elements of 1st martix \n");
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      scanf("%d", &arr[i][j]);
+    }
+  }
+  printf("Enter elements of 2nd martix \n");
+  for (int i = 0; i < p; i++)
+  {
+    for (int j = 0; j < q; j++)
+    {
+      scanf("%d", &brr[i][j]);
+    }
+  }
+}
+void get_dimension(int *m, int *n, int *p, int *q)
+{
+  // 1st matrix
+  printf("row of 1st matrix : ");
+  scanf("%d", m);
+  printf("column of 1st matrix : ");
+  scanf("%d", n);
+  // 2nd matrix
+  printf("row of 2nd matrix : ");
+  scanf("%d", p);
+  printf("column of 2nd matrix : ");
+  scanf("%d", q);
+}
+void print_array(int res[m][q])
+{
+  printf("the result of multiplication.....\n");
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < q; j++)
+    {
+      printf("%d ", res[i][j]);
+    }
+    printf("\n");
+  }
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,30 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void sum_of_rows(int arr[][3], int rows, int cols);
+int m, n, p, q;
+
+void get_dimension(int *m, int *n, int *p, int *q);
+void print_array(int **res, int r, int c);
+void matrix_multiply(int **arr, int **brr, int **res, int r, int c, int cr);
 
 int main()
 {
-  int arr[2][3] = {{1, 4, 3}, {2, 4, 1}};
-  sum_of_rows(arr, 2, 3);
+  get_dimension(&m, &n, &p, &q);
+  printf("%d %d %d %d\n", m, n, p, q);
+  if (n != p)
+  {
+    printf("The matrices cannot be multiplied");
+    return 0;
+  }
+  // Allocate memory for the arrays
+  int **arr = (int **)malloc(m * sizeof(int *));
+  for (int i = 0; i < m; i++)
+    arr[i] = (int *)malloc(n * sizeof(int));
+
+  int **brr = (int **)malloc(p * sizeof(int *));
+  for (int i = 0; i < p; i++)
+    brr[i] = (int *)malloc(q * sizeof(int));
+
+  int **res = (int **)malloc(m * sizeof(int *));
+  for (int i = 0; i < m; i++)
+    res[i] = (int *)malloc(q * sizeof(int));
+
+  // Initialize arr and brr with some values
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      arr[i][j] = i * n + j + 1;
+
+  for (int i = 0; i < p; i++)
+    for (int j = 0; j < q; j++)
+      brr[i][j] = i * q + j + 1;
+
+  int cr = n; // arr[][c] & brr[][r]
+  matrix_multiply(arr, brr, res, m, q, cr);
+  print_array(res, m, q);
+
+  // Free allocated memory
+  for (int i = 0; i < m; i++)
+    free(arr[i]);
+  free(arr);
+
+  for (int i = 0; i < p; i++)
+    free(brr[i]);
+  free(brr);
+
+  for (int i = 0; i < m; i++)
+    free(res[i]);
+  free(res);
+
   return 0;
 }
-void sum_of_rows(int arr[][3], int rows, int cols)
+
+void get_dimension(int *m, int *n, int *p, int *q)
 {
-  int max_sum = 0;
-  int max_row = 0;
-  for (int i = 0; i < rows; i++)
+  // 1st matrix
+  printf("row of 1st matrix : ");
+  scanf("%d", m);
+  printf("column of 1st matrix : ");
+  scanf("%d", n);
+  // 2nd matrix
+  printf("row of 2nd matrix : ");
+  scanf("%d", p);
+  printf("column of 2nd matrix : ");
+  scanf("%d", q);
+}
+
+void matrix_multiply(int **arr, int **brr, int **res, int r, int c, int cr)
+{
+  for (int i = 0; i < r; i++)
   {
-    int sum_row = 0;
-    for (int j = 0; j < cols; j++)
+    for (int j = 0; j < c; j++)
     {
-      sum_row += arr[i][j];
-    }
-    printf("Sum of row %d is: %d\n", i + 1, sum_row);
-    if (sum_row > max_sum)
-    {
-      max_sum = sum_row;
-      max_row = i+1;
+      res[i][j] = 0;
+      for (int k = 0; k < cr; k++)
+      {
+        res[i][j] += arr[i][k] * brr[k][j];
+      }
     }
   }
-  printf("The row with the maximum sum is: %d\nthe sum is : %d", max_row, max_sum);
+}
+
+void print_array(int **res, int r, int c)
+{
+  for (int i = 0; i < r; i++)
+  {
+    for (int j = 0; j < c; j++)
+    {
+      printf("%d ", res[i][j]);
+    }
+    printf("\n");
+  }
 }
