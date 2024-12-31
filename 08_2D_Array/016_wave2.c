ee643068da46954f7a1fde1334171a6012e34fc9
RAHUL144 <117508363+rahulsaha1045451444@users.noreply.github.com>
2024-12-31 20:21:12 +0530

    g fd

--- 08_2D_Array/016_wave2.c
+++ 08_2D_Array/016_wave2.c
@@ -1,25 +1,53 @@
 #include <stdio.h>
 /* Main Function */
+void reverse_even_column_direct_print(int arr[3][3]);
+void reverse_even_column(int arr[3][3]);
 void print_array(int arr[3][3]);
 int main()
 {
   int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  for (int i = 0, k = 3 - 1; i < 3; i++)
+  // reverse_even_column_direct_print(arr);
+  print_array(arr);
+  puts("");
+  reverse_even_column(arr);
+  print_array(arr);
+  return 0;
+}
+void reverse_even_column(int arr[3][3])
+{
+  for (int j = 0; j < 3; j++)
   {
-    for (int j = 0; j < 3; j++)
+    if (j % 2 == 0)
     {
-      if (j % 2 == 0)
+      for (int i = 0, k = 3 - 1; i <= k; i++, k--)
       {
         int temp = arr[i][j];
         arr[i][j] = arr[k][j];
         arr[k][j] = temp;
-        k--;
       }
     }
   }
-  print_array(arr);
-  // puts("");
-  return 0;
+}
+void reverse_even_column_direct_print(int arr[3][3])
+{
+  for (int j = 0; j < 3; j++)
+  {
+    if (j % 2 == 0)
+    {
+      for (int i = 3 - 1; i >= 0; i--)
+      {
+        printf("%d ", arr[i][j]);
+      }
+    }
+    else
+    {
+      for (int i = 0; i < 3; i++)
+      {
+        printf("%d ", arr[i][j]);
+      }
+    }
+    puts("");
+  }
 }
 void print_array(int arr[3][3])
 {
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,99 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int m, n, p, q;
+void print_array(int arr[4][3]);
 
-void get_dimension(int *m, int *n, int *p, int *q);
-void print_array(int **res, int r, int c);
-void matrix_multiply(int **arr, int **brr, int **res, int r, int c, int cr);
-
+/* Main Function */
 int main()
 {
-  get_dimension(&m, &n, &p, &q);
-  printf("%d %d %d %d\n", m, n, p, q);
-  if (n != p)
+  int arr[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+  
+  // Print the array
+  printf("Original array:\n");
+  print_array(arr);
+  
+  // Print columns in reverse order
+  printf("Modified array print:\n");
+  for (int j = 0; j < 3; j++)
   {
-    printf("The matrices cannot be multiplied");
-    return 0;
+    for (int i = 3; i >= 0; i--)
+    {
+      printf("%d ", arr[i][j]);
+    }
+    printf("\n");
   }
-  // Allocate memory for the arrays
-  int **arr = (int **)malloc(m * sizeof(int *));
-  for (int i = 0; i < m; i++)
-    arr[i] = (int *)malloc(n * sizeof(int));
-
-  int **brr = (int **)malloc(p * sizeof(int *));
-  for (int i = 0; i < p; i++)
-    brr[i] = (int *)malloc(q * sizeof(int));
-
-  int **res = (int **)malloc(m * sizeof(int *));
-  for (int i = 0; i < m; i++)
-    res[i] = (int *)malloc(q * sizeof(int));
-
-  // Initialize arr and brr with some values
-  for (int i = 0; i < m; i++)
-    for (int j = 0; j < n; j++)
-      arr[i][j] = i * n + j + 1;
-
-  for (int i = 0; i < p; i++)
-    for (int j = 0; j < q; j++)
-      brr[i][j] = i * q + j + 1;
-
-  int cr = n; // arr[][c] & brr[][r]
-  matrix_multiply(arr, brr, res, m, q, cr);
-  print_array(res, m, q);
-
-  // Free allocated memory
-  for (int i = 0; i < m; i++)
-    free(arr[i]);
-  free(arr);
-
-  for (int i = 0; i < p; i++)
-    free(brr[i]);
-  free(brr);
-
-  for (int i = 0; i < m; i++)
-    free(res[i]);
-  free(res);
-
+  
   return 0;
 }
 
-void get_dimension(int *m, int *n, int *p, int *q)
+void print_array(int arr[4][3])
 {
-  // 1st matrix
-  printf("row of 1st matrix : ");
-  scanf("%d", m);
-  printf("column of 1st matrix : ");
-  scanf("%d", n);
-  // 2nd matrix
-  printf("row of 2nd matrix : ");
-  scanf("%d", p);
-  printf("column of 2nd matrix : ");
-  scanf("%d", q);
-}
-
-void matrix_multiply(int **arr, int **brr, int **res, int r, int c, int cr)
-{
-  for (int i = 0; i < r; i++)
+  for (int i = 0; i < 4; i++)
   {
-    for (int j = 0; j < c; j++)
+    for (int j = 0; j < 3; j++)
     {
-      res[i][j] = 0;
-      for (int k = 0; k < cr; k++)
-      {
-        res[i][j] += arr[i][k] * brr[k][j];
-      }
-    }
-  }
-}
-
-void print_array(int **res, int r, int c)
-{
-  for (int i = 0; i < r; i++)
-  {
-    for (int j = 0; j < c; j++)
-    {
-      printf("%d ", res[i][j]);
+      printf("%d ", arr[i][j]);
     }
     printf("\n");
   }
